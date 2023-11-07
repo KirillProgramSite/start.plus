cad513dc04215b48adcb909eed0a564846102315
KirillProgramSite <kposkuri@gmail.com>
2023-11-08 01:10:13 +0300

    remote commit

--- main.js
+++ main.js
@@ -4,6 +4,12 @@ let main = document.querySelector('.main');
 
 menuBtn.addEventListener("click", () => {
     nav__mobile.classList.toggle('open_menu')
+
+    if(nav__mobile.classList.contains('open_menu')){
+        menuBtn.style.opacity = 0.5;
+    } else {
+        menuBtn.style.opacity = 1;
+    }
 })
 
 main.addEventListener("click", () => {
@@ -13,4 +19,10 @@ main.addEventListener("click", () => {
 main.addEventListener("touchmove", () => {
     nav__mobile.classList.remove('open_menu')
 
-})  
+    if(nav__mobile.classList.contains('open_menu')){
+        menuBtn.style.opacity = 0.5;
+    } else {
+        menuBtn.style.opacity = 1;
+    }
+
+})
