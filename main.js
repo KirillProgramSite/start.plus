cb99380fb464221d45b7cf6be80e633182b5a3f8
KirillProgramSite <kposkuri@gmail.com>
2023-11-09 13:33:50 +0300

    fixed adaptive

--- main.js
+++ main.js
@@ -5,25 +5,30 @@ let main = document.querySelector('.main');
 function opacityMode() {
     if(nav__mobile.classList.contains('open_menu')){
         menuBtn.style.opacity = 0.5;
+        main.style.opacity = 0.5;
     } else {
         menuBtn.style.opacity = 1;
+        main.style.opacity = 1;
     }
 }
 
 menuBtn.addEventListener("click", () => {
     nav__mobile.classList.toggle('open_menu')
+    nav__mobile.classList.toggle('anim_shadow')
 
    opacityMode()
 })
 
 main.addEventListener("click", () => {
     nav__mobile.classList.remove('open_menu')
+    nav__mobile.classList.remove('anim_shadow')
 
     opacityMode()
 })
 
 main.addEventListener("touchmove", () => {
     nav__mobile.classList.remove('open_menu')
+    nav__mobile.classList.remove('anim_shadow')
 
     opacityMode()
 })
